78e133e759a52cb6161bcea877591b00c4a801b4
506boris <506boris@gmail.com>
2018-05-28 12:23:22 +0300

    Add files via upload

---
+++ desktop/src/by/vit/boombony/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package by.vit.boombony.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import by.vit.boombony.BoombonyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BoombonyGame(), config);
+	}
+}
