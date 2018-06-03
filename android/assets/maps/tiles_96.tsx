5104d7d62cf25912eac27d1ac669023522cde2d9
506boris <506boris@gmail.com>
2018-06-03 12:13:08 +0300

    add missing textures

---
+++ android/assets/maps/tiles_96.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles_96" tilewidth="96" tileheight="96" tilecount="64" columns="8">
+ <image source="tiles_96.png" width="768" height="768"/>
+ <tile id="0">
+  <properties>
+   <property name="TYPE" value="GRASS"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="TYPE" value="CURSOR"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="TYPE" value="GOLD_STONE"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="TYPE" value="STONE"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="TYPE" value="BRICK"/>
+  </properties>
+ </tile>
+</tileset>
